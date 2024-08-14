05373f1d2d46cc9ad244bfbd08285b6b6908e2bc
vishnu26r <vishnuvel26.r@gmail.com>
2024-08-14 22:06:06 +0530

    i

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import Layout from './components/layout';
 import Home from './pages/home';
 import Detail from './pages/detail';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 function App() {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/:slug' element={<Detail />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-    
   );
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,17 @@
 import React from 'react'
-
-const Home = ()=>{
-    return(
-        <div>Home</div>
-    )
-    
+import { products } from '../products'
+import ProductCart from '../components/productCart'
+const Home = () => {
+  return (
+    <div>
+        <h1 className='text-3xl my-5'>List Products</h1>
+        <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-5'>
+            {products.map((product, key) => 
+                <ProductCart key={key} data={product}/>
+            )}
+        </div>
+    </div>
+  )
 }
+
 export default Home
